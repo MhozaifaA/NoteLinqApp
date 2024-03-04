a3e4d739a9d745c7ed0ec6762bfff45de315d013
MhozaifaA <botchey.mha@hotmail.com>
2024-03-04 23:50:03 +0400

    Note and classification controller

---
+++ Applications/WebAPIs/NoteLinqAppAPI/Controllers/ClassificationController.cs
@@ -0,0 +1,25 @@
+﻿using Meteors;
+using Meteors.AspNetCore.MVC;
+using Meteors.AspNetCore.MVC.Attributes;
+using Meteors.AspNetCore.MVC.Security.Attribute;
+using Microsoft.AspNetCore.Mvc;
+using NoteLinqApp;
+using NoteLinqApp.BoundedContext.DataTransferObjects;
+using NoteLinqApp.Domain;
+
+namespace NoteLinqAppAPI.Controllers
+{
+    [ApiController, AuthorizeJwt, DefaultRoute]
+    public class ClassificationController : MrControllerBaseGeneral<Guid, IClassificationService, ClassificationDto>
+    {
+        public ClassificationController(IClassificationService repository) : base(repository)
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            return await repository.List().ToJsonResultAsync();
+        }
+    }
+}
---
+++ Applications/WebAPIs/NoteLinqAppAPI/Controllers/NoteController.cs
@@ -0,0 +1,45 @@
+﻿using Meteors.AspNetCore.MVC.Attributes;
+using Meteors.AspNetCore.MVC;
+using Meteors;
+using Microsoft.AspNetCore.Mvc;
+using NoteLinqApp.BoundedContext.DataTransferObjects;
+using NoteLinqApp.Domain;
+using NoteLinqApp;
+using System.ComponentModel.DataAnnotations;
+
+namespace NoteLinqAppAPI.Controllers
+{
+
+    [ApiController, AuthorizeJwt, DefaultRoute]
+    public class NoteController : MrControllerBase<Guid, INoteService>
+    {
+        public NoteController(INoteService repository) : base(repository)
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> List()
+        {
+            return await repository.List().ToJsonResultAsync();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody][Required] NoteDto dto)
+        {
+            return await repository.AddAsync(dto).ToJsonResultAsync();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Modify([FromBody][Required] NoteDto dto)
+        {
+            return await repository.ModifyAsync(dto).ToJsonResultAsync();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute][Required] Guid id)
+        {
+            return await repository.DeleteAsync(id).ToJsonResultAsync();
+        }
+
+    }
+}
