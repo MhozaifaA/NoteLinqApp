78880db534b97677484a05d765416ce6dd1d77d6
MhozaifaA <botchey.mha@hotmail.com>
2024-03-04 23:44:17 +0400

    Create simple Account repo/ add IData domain

---
+++ BoundedContext/Repositories/NoteLinqApp.BoundedContext.Repositories.Security/Provider/AssemblyProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace NoteLinqApp.BoundedContext.Repositories.Security;
+
+public class BoundedContextRepositoriesSecurityAssembly
+{
+    public static Assembly Assembly = Assembly.GetAssembly(typeof(BoundedContextRepositoriesSecurityAssembly));
+}
+
---
+++ Domain/NoteLinqApp.Domain/IData/Repositoreis/Security/IAccountRepository.cs
@@ -0,0 +1,17 @@
+﻿using Meteors;
+using Meteors.AspNetCore.Service.BoundedContext;
+using NorLinqApp.DataTransferObjects.Security;
+using NoteLinqApp.DataTransferObjects.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteLinqApp.Domain
+{
+    public interface IAccountRepository : IMrRepository
+    {
+        Task<OperationResult<AccessTokenDto>> Login(LoginDto dto);
+    }
+}
