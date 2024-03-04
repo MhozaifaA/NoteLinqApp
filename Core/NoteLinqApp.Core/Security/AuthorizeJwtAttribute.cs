14bb951f57f14fbe5911456ccf1d9148c75d0b8a
MhozaifaA <botchey.mha@hotmail.com>
2024-03-04 23:36:24 +0400

    Add helper Attribute inh from Mr AuthenticationScheme

---
+++ Core/NoteLinqApp.Core/Security/AuthorizeJwtAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Meteors.AspNetCore.MVC.Security.Attribute;
+
+namespace NoteLinqApp
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class AuthorizeJwtAttribute : MrAuthorizeAttribute
+    {
+        public AuthorizeJwtAttribute()
+        {
+            AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme;
+        }
+    }
+}
