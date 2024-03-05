825f006229c9038bb78fd4e3e614eb05c1220935
MhozaifaA <botchey.mha@hotmail.com>
2024-03-06 00:40:26 +0400

    Add login view

---
+++ Applications/Presentation/NoteLinqApp/src/views/security/LoginView.vue
@@ -0,0 +1,210 @@
+﻿
+<template>
+    <main class="d-flex align-items-center min-vh-100 py-3 py-md-0">
+        <div class="container">
+
+            <div class="card login-card">
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="/src/assets/images/login.jpg" alt="login" class="login-card-img">
+                    </div>
+
+                    <div class="col-md-6">
+                        <div class="card-body text-center">
+
+                            <div class="brand-wrapper mt-5">
+                                <img width="100" src="/src/assets/images/logo.png" alt="logo" class="logo">
+                            </div>
+
+                            <p class="login-card-description">Sign into your account</p>
+                            <form @submit.prevent="login" class="was-validated justify-content-center d-inline-block">
+
+                                <div class="form-group mb-2">
+                                    <input type="text" required
+                                           name="username" v-model.trim="model.userName"
+                                           class="form-control" placeholder="User name">
+                                </div>
+                                <div class="form-group mb-4">
+                                    <input type="password" required name="password" v-model.trim="model.password"
+                                           class="form-control" placeholder="●●●●●●●●">
+                                </div>
+                                <input name="login" id="login" class="btn btn-dark login-btn mb-4 w-100" type="submit" value="Login">
+
+                            </form>
+                            <nav class="login-card-footer-nav justify-content-between d-flex">
+                                <a href="https://norlinq.com/">Terms & Conditions.</a>
+                                <a href="https://norlinq.com/">Privacy policy</a>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+</template>
+
+<script setup lang="ts">
+    import { onMounted, ref } from "vue";
+    import type { AccessTokenDto, LoginDto } from "../../core/models/SecurityDtos";
+    import Storage from "../../core/services/storage.service"
+    import { LoginUrl } from "../../core/appsettings";
+    import HttpAppService from '../../core/services/http.service';
+    import { useRouter } from "vue-router";
+
+    //Storage.Flush();
+
+    const router = useRouter();
+
+    const model = ref<LoginDto>({ userName: "user", password: "user", rememberMe: true, });
+
+
+    onMounted(() => {
+        Storage.Flush();
+    })
+
+    async function login() {
+
+        (await HttpAppService._Post<AccessTokenDto>
+            (LoginUrl, model.value)).
+            Result(dto => {
+                if (dto) {
+                    Storage.SetCurrentAccount(dto);
+                    if (dto.accessToken) {
+                       Storage.SetAccessToken(dto.accessToken);
+                        router.push('/');
+                    }
+                }
+            }).Error(err => console.error(err));
+      
+    }
+
+
+</script>
+
+
+<style scoped>
+
+    body {
+        background-color: #d0d0ce;
+        min-height: 100vh;
+    }
+
+    main {
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-image: linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5)), url('/src/assets/images/login.jpg');
+        background-position: 50% 0;
+    }
+
+    .brand-wrapper.logo {
+        height: 80px;
+    }
+
+    .login-card {
+        border: 0;
+        border-radius: 27.5px;
+        box-shadow: 0 10px 30px 0 rgba(172, 168, 168, 0.43);
+        overflow: hidden;
+    }
+
+    .login-card-img {
+        border-radius: 0;
+        /* position: absolute;*/
+        width: 100%;
+        height: 100%;
+        -o-object-fit: cover;
+        object-fit: cover;
+    }
+
+    .login-card.card-body {
+        padding: 85px 60px 60px;
+    }
+
+    @media(max-width: 422px) {
+        .login-card.card-body {
+            padding: 35px 24px;
+        }
+    }
+
+    .login-card-description {
+        font-size: 25px;
+        color: #000;
+        font-weight: normal;
+        margin-bottom: 23px;
+    }
+
+    .login-card form {
+        max-width: 326px;
+    }
+
+    .login-card.form-control {
+        border: 1px solid #d5dae2;
+        padding: 15px 25px;
+        margin-bottom: 20px;
+        min-height: 45px;
+        font-size: 13px;
+        /*  line-height: 15;*/
+        font-weight: normal;
+    }
+
+        .login-card.form-control::-webkit-input-placeholder {
+            color: #919aa3;
+        }
+
+        .login-card.form-control::-moz-placeholder {
+            color: #919aa3;
+        }
+
+        .login-card.form-control:-ms-input-placeholder {
+            color: #919aa3;
+        }
+
+        .login-card.form-control:-ms-input-placeholder {
+            color: #919aa3;
+        }
+
+        .login-card.form-control::placeholder {
+            color: #919aa3;
+        }
+
+    .login-card.login-btn {
+        padding: 13px 20px 12px;
+        background-color: #000;
+        border-radius: 4px;
+        font-size: 17px;
+        font-weight: bold;
+        line-height: 20px;
+        color: #fff;
+        margin-bottom: 24px;
+    }
+
+        .login-card.login-btn:hover {
+            border: 1px solid #000;
+            background-color: transparent;
+            color: #000;
+        }
+
+    .login-card.forgot-password-link {
+        font-size: 14px;
+        color: #919aa3;
+        margin-bottom: 12px;
+    }
+
+    .login-card-footer-text {
+        font-size: 16px;
+        color: #0d2366;
+        margin-bottom: 60px;
+    }
+
+    @media(max-width: 767px) {
+        .login-card-footer-text {
+            margin-bottom: 24px;
+        }
+    }
+
+    .login-card-footer-nav a {
+        font-size: 14px;
+        color: #919aa3;
+    }
+</style>
---
+++ Applications/Presentation/NoteLinqApp/src/views/shared/layouts/Layout.vue
@@ -0,0 +1,144 @@
+﻿
+<template>
+
+    <div class="container-fluid p-0 d-flex h-100">
+
+        <div id="mainsidebar"
+             class="d-flex flex-column
+                    flex-shrink-0 p-3
+                    text-white offcanvas-md offcanvas-start">
+            <RouterLink to="/" class="navbar-brand text-center">
+
+                <img width="80" src="/src/assets/images/logo.png" />
+
+            </RouterLink>
+
+            <hr>
+            <ul class="custnav text-dark nav nav-pills flex-column mb-auto">
+
+                <li class="nav-item mb-1">
+                    <RouterLink to="/notes" active-class="active" class="p-3 rounded-2 d-block w-100 text-decoration-none text-dark fs-5" href="#">
+                        <i class="me-2 bi bi-people"></i>
+                        Notes
+                    </RouterLink>
+                </li>
+
+
+                <li class="nav-item mb-1">
+                    <RouterLink to="/settings" active-class="active" class="p-3 rounded-2 d-block w-100 text-decoration-none text-dark fs-5" href="#">
+                        <i class="me-2 bi bi-gear"></i>
+                        Settings
+                    </RouterLink>
+                </li>
+
+
+
+
+            </ul>
+
+            <hr>
+
+            <div class="d-flex">
+
+                <span>
+                    <h6 class="text-dark">
+                        {{Storage.GetCurrentAccount()?.email}}
+                    </h6>
+                </span>
+                <i class="bi bi-emoji-smile-fill text-dark ms-2"></i>
+            </div>
+
+        </div>
+
+        <div class="bg-light flex-fill">
+
+
+
+            <div class="p-4 pt-2">
+
+                <nav class="navbar bg-light p-0">
+                    <div class="container-fluid">
+
+
+                        <div class="p-2 d-md-none d-flex text-dark ">
+                            <a href="#" class="text-dark"
+                               data-bs-toggle="offcanvas"
+                               data-bs-target="#mainsidebar">
+                                <i class="bi bi-list"></i>
+                            </a>
+                            <span class="ms-3">Note Linq App</span>
+                        </div>
+
+
+                        <a class="navbar-brand col">Portal</a>
+
+
+                        <div class="d-flex align-items-center dropdown">
+                            <span class="mx-2">Hi, {{Storage.GetCurrentAccount()?.name}}</span>
+
+                            <button class="btn border-0 dropdown-toggle content"
+                                    type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <i class="fs-3 bi bi-person-circle"></i>
+                            </button>
+                            <ul class="dropdown-menu dropdown-menu-dark">
+                                <li><h6 class="dropdown-header">Profile</h6></li>
+                                <li><RouterLink class="dropdown-item" to="/login">logout</RouterLink></li>
+                            </ul>
+
+                        </div>
+
+                    </div>
+                </nav>
+
+                <hr class="my-2">
+                <div class="row">
+                    <div class="col">
+
+                        <RouterView />
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+    import { onMounted } from 'vue';
+    import { useRouter } from "vue-router";
+    import Storage from "../../../core/services/storage.service"
+
+
+    const router = useRouter();
+
+    if (!Storage.IsAuthorize()) {
+        Storage.Flush();
+        router.push("login")
+    }
+
+</script>
+
+<style>
+
+    html, body {
+        height: 100%;
+    }
+
+
+    .custnav li a.active {
+        background: rgba(0, 0, 0, 0.2);
+    }
+
+    .custnav li a:hover {
+        background: rgba(0, 0, 0, 0.2);
+        transition: 0.8s
+    }
+
+
+    .dropdown-toggle::after {
+        display: none !important;
+        content: unset !important;
+    }
+</style>
